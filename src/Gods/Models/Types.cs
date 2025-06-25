2ea3f7308b3a2c6b8299fea9f891f3da2f8f0bbc
Maik Müller <15815012+aatmmr@users.noreply.github.com>
2025-06-25 09:36:40 +0200

    adding new mythologies

--- src/Gods/Models/Types.cs
+++ src/Gods/Models/Types.cs
@@ -1,8 +1,14 @@
 
 namespace MythApi.Gods.Models;
 
-public enum Mythology {
+public enum Mythology
+{
     Norse = 1,
     Greek = 2,
-    Roman = 3
+    Roman = 3,
+    Egyptian = 4,
+    Hindu = 5,
+    Celtic = 6,
+    Japanese = 7,
+    Chinese = 8
 }
